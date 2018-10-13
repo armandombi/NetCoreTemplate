7358ba5be6f16b56be6927b6e5f6ee4eea71f4ea
Armando Bracho <armandombi@gmail.com>
2018-10-13 12:47:45 +0100

    adding popper and jquery for dropdown functionality

--- wwwroot/source/app.js
+++ wwwroot/source/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Message from './components/Message';
+import jquery from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 ReactDOM.render(
