594b80085e8add214898f6df3be1f1440980ec0a
Armando Bracho <armandombi@gmail.com>
2018-10-13 22:59:15 +0100

    Adding jumbotron and theme component and refactoring

--- wwwroot/source/app.js
+++ wwwroot/source/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Message from './components/Message';
+import Theme from './components/Theme';
 import jquery from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 ReactDOM.render(
-   <Message />,
+   <Theme />,
    document.getElementById('root')
 );
 module.hot.accept();
